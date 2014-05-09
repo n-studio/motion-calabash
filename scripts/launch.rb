1ad273f885dcbf9b8ca10b8b1d6efc4e1276371f
n-studio <contact@n-studio.fr>
2014-05-09 09:44:56 +0900

    added static library

--- scripts/launch.rb
+++ scripts/launch.rb
@@ -24,12 +24,18 @@
 require 'calabash-cucumber/launch/simulator_helper'
 require 'sim_launcher'
 require 'calabash-cucumber/device'
+require 'calabash-cucumber/actions/instruments_actions'
+require 'calabash-cucumber/actions/playback_actions'
 require 'run_loop'
 require 'cfpropertylist'
 
 class Calabash::Cucumber::Launcher
+
+  KNOWN_PRIVACY_SETTINGS = {:photos => 'kTCCServicePhotos', :calendar => 'kTCCServiceCalendar', :address_book => 'kTCCServiceAddressBook'}
+
   attr_accessor :run_loop
   attr_accessor :device
+  attr_accessor :actions
   attr_accessor :launch_args
 
   @@launcher = nil
@@ -49,8 +55,62 @@ class Calabash::Cucumber::Launcher
   class CalabashLauncherTimeoutErr < Timeout::Error
   end
 
+
+  def actions
+    attach if @actions.nil?
+    @actions
+  end
+
+  def self.attach
+    l = launcher
+    return l if l && l.active?
+    l.attach
+
+  end
+
+  def attach(max_retry=1, timeout=10)
+    if calabash_no_launch?
+      self.actions= Calabash::Cucumber::PlaybackActions.new
+      return
+    end
+
+    pids_str = `ps x -o pid,command | grep -v grep | grep "instruments" | awk '{printf "%s,", $1}'`
+    pids = pids_str.split(',').map { |pid| pid.to_i }
+    pid = pids.first
+    rl = {}
+    if pid
+      rl[:pid] = pid
+      self.actions= Calabash::Cucumber::InstrumentsActions.new
+    else
+      self.actions= Calabash::Cucumber::PlaybackActions.new
+    end
+
+    self.run_loop= rl
+
+    ensure_connectivity(max_retry, timeout)
+
+    major = self.device.ios_major_version
+    if major.to_i >= 7 && self.actions.is_a?(Calabash::Cucumber::PlaybackActions)
+      puts "\n\n WARNING \n\n"
+      puts 'Warning Trying to connect to simulator that was not launched by Calabash/instruments.'
+      puts 'To fix this you must let Calabash or instruments launch the app'
+      puts 'Continuing... query et al will work.'
+      puts "\n\n WARNING \n\n"
+      puts "Please read: https://github.com/calabash/calabash-ios/wiki/A0-UIAutomation---instruments-problems"
+    end
+
+
+    self
+  end
+
+  def self.instruments?
+    l = launcher_if_used
+    return false unless l
+    l.instruments?
+  end
+
   def self.launcher
-    @@launcher ||= Launcher.new
+    @@launcher ||= Calabash::Cucumber::Launcher.new
   end
 
   def self.launcher_if_used
@@ -76,13 +136,84 @@ class Calabash::Cucumber::Launcher
     path ||= Calabash::Cucumber::SimulatorHelper.app_bundle_or_raise(app_path)
 
     app = File.basename(path)
-    bundle = `find "#{ENV['HOME']}/Library/Application Support/iPhone Simulator/#{sdk}/Applications/" -type d -depth 2 -name "#{app}" | head -n 1`
-    return if bundle.empty? # Assuming we're already clean
-
-    sandbox = File.dirname(bundle)
-    ['Library', 'Documents', 'tmp'].each do |dir|
-      FileUtils.rm_rf(File.join(sandbox, dir))
+    directories_for_sdk_prefix(sdk).each do |dir|
+      bundle = `find "#{dir}/Applications" -type d -depth 2 -name "#{app}" | head -n 1`
+      next if bundle.empty? # Assuming we're already clean
+      if ENV['DEBUG']=='1'
+        puts "Reset app state for #{bundle}"
+      end
+      sandbox = File.dirname(bundle)
+      ['Library', 'Documents', 'tmp'].each do |dir|
+        FileUtils.rm_rf(File.join(sandbox, dir))
+      end
     end
+
+
+  end
+
+  def directories_for_sdk_prefix(sdk)
+    Dir["#{ENV['HOME']}/Library/Application Support/iPhone Simulator/#{sdk}*"]
+  end
+
+  # Call as update_privacy_settings('com.my.app', {:photos => {:allow => true}})
+  def update_privacy_settings(bundle_id, opts={})
+    if ENV['DEBUG']=='1'
+      puts "Update privacy settings #{bundle_id}, #{opts}"
+    end
+    unless File.exist?(`which sqlite3`.strip)
+      raise 'Error: Unable to find sqlite3. The binary sqlite3 must be installed and on path.'
+    end
+    opts.each do |setting_name, setting_options|
+
+      setting_name = KNOWN_PRIVACY_SETTINGS[setting_name] || setting_name
+      allow = setting_options[:allow] == false ? false : true
+      sdk = setting_options[:sdk] || SimLauncher::SdkDetector.new().latest_sdk_version
+
+      dirs = directories_for_sdk_prefix(sdk)
+      if ENV['DEBUG']=='1'
+        puts "About to update privacy setting #{setting_name} for #{bundle_id}, allow: #{allow} in sdk #{sdk}, #{dirs}"
+      end
+
+      dirs.each do |dir|
+        if ENV['DEBUG']=='1'
+          puts "Setting access for #{bundle_id} for permission #{setting_name} to allow: #{allow}"
+        end
+        path_to_tcc_db = tcc_database_for_sdk_dir(dir)
+        unless File.exist?(path_to_tcc_db)
+          puts "Warning: No TCC.db in location #{path_to_tcc_db}"
+          next
+        end
+        allowed_as_i = allow ? 1 : 0
+        if privacy_setting(dir, bundle_id,setting_name).nil?
+          sql = %Q['INSERT INTO access (service, client, client_type, allowed, prompt_count) VALUES ("#{setting_name}","#{bundle_id}",0,#{allowed_as_i},1);']
+        else
+          sql = %Q['UPDATE access SET allowed=#{allowed_as_i} where client="#{bundle_id}" AND service="#{setting_name}";']
+        end
+
+        if ENV['DEBUG']=='1'
+          puts "Executing sql #{sql} on #{path_to_tcc_db}"
+        end
+
+        unless system(%Q[sqlite3 "#{path_to_tcc_db}" #{sql}]) && privacy_setting(dir,bundle_id,setting_name) == allowed_as_i
+          puts "Warning: Error executing sql: #{sql} against #{path_to_tcc_db} (Setting is #{privacy_setting(dir,bundle_id,setting_name)}). Continuing..."
+          next
+        end
+      end
+    end
+
+  end
+
+  def tcc_database_for_sdk_dir(dir)
+    File.join(dir,'Library', 'TCC', 'TCC.db')
+  end
+
+  def privacy_setting(sdk_dir, bundle_id, setting_name)
+    setting_name = KNOWN_PRIVACY_SETTINGS[setting_name] || setting_name
+    path_to_tcc_db = tcc_database_for_sdk_dir(sdk_dir)
+    sql = %Q['SELECT allowed FROM access WHERE client="#{bundle_id}" and service="#{setting_name}";']
+    output = `sqlite3 "#{path_to_tcc_db}" #{sql}`.strip
+
+    (output == '0' || output == '1') ? output.to_i : nil
   end
 
   def default_launch_args
@@ -123,17 +254,45 @@ class Calabash::Cucumber::Launcher
       args[:device_target] = 'simulator'
     end
 
-
     args
   end
 
   def detect_connected_device?
+    if ENV['DETECT_CONNECTED_DEVICE'] == '1'
+      return true
+    end
+
+    if ENV['BUNDLE_ID'].nil? && ENV['DETECT_CONNECTED_DEVICE'].nil?
+      return false
+    end
+    if ENV['BUNDLE_ID'] && ENV['DETECT_CONNECTED_DEVICE'].nil?
+      return true
+    end
+    if ENV['DETECT_CONNECTED_DEVICE']
       return ENV['DETECT_CONNECTED_DEVICE'] != '0'
+    end
+
+    return false
   end
 
   def default_launch_method
-    return :instruments unless sdk_version || use_instruments_env?
-    return :instruments if sdk_version.start_with?('7') # Only instruments supported for iOS7+
+    sdk = sdk_version
+    major = nil
+    if sdk && !sdk.strip.empty?
+      major = sdk.split('.')[0]
+      begin
+        major = major.to_i
+      rescue
+        warn("SDK_VERSION invalid #{sdk_version} - ignoring...")
+      end
+    end
+    return :instruments if major && major >= 7 # Only instruments supported for iOS7+
+    return :sim_launcher if major # and then we have <= 6
+
+    if RunLoop::Core.above_or_eql_version?('5.1', RunLoop::Core.xcode_version)
+      return use_sim_launcher_env? ? :sim_launcher : :instruments
+    end
+
     sim_detector = SimLauncher::SdkDetector.new()
     available = sim_detector.available_sdk_versions.reject { |v| v.start_with?('7') }
     if available.include?(sdk_version)
@@ -178,12 +337,24 @@ class Calabash::Cucumber::Launcher
 
     reset_app_jail if args[:reset]
 
+
+    if args[:privacy_settings]
+      if args[:device_target]=='simulator'
+        update_privacy_settings(args[:bundle_id], args[:privacy_settings])
+      else
+        #Not supported on device
+        puts 'Warning: :privacy_settings not supported on device'
+      end
+    end
+
     if run_with_instruments?(args)
       self.run_loop = new_run_loop(args)
+      self.actions= Calabash::Cucumber::InstrumentsActions.new
     else
       # run with sim launcher
       sdk = sdk_version || SimLauncher::SdkDetector.new().available_sdk_versions.reverse.find { |x| !x.start_with?('7') }
       path = Calabash::Cucumber::SimulatorHelper.app_bundle_or_raise(app_path)
+      self.actions= Calabash::Cucumber::PlaybackActions.new
       Calabash::Cucumber::SimulatorHelper.relaunch(path, sdk, args[:device].to_s, args)
     end
     self.launch_args = args
@@ -239,24 +410,30 @@ class Calabash::Cucumber::Launcher
   end
 
   def new_run_loop(args)
+    if RunLoop::Core.above_or_eql_version?('5.1', RunLoop::Core.xcode_version)
+      Calabash::Cucumber::SimulatorHelper.stop
+    end
     last_err = nil
-    3.times do
+    5.times do
       begin
         return RunLoop.run(args)
       rescue RunLoop::TimeoutError => e
         last_err = e
         if ENV['CALABASH_FULL_CONSOLE_OUTPUT'] == '1'
-          puts "retrying run loop..."
+          puts 'retrying run loop...'
         end
+        Calabash::Cucumber::SimulatorHelper.stop
       end
     end
+    Calabash::Cucumber::SimulatorHelper.stop
+    puts "Unable to start. Make sure you've set APP_BUNDLE_PATH to a build supported by this simulator version"
     raise StartError.new(last_err)
   end
 
-  def ensure_connectivity
+  def ensure_connectivity(max_retry=10, timeout=30)
     begin
-      max_retry_count = (ENV['MAX_CONNECT_RETRY'] || 10).to_i
-      timeout = (ENV['CONNECT_TIMEOUT'] || 30).to_i
+      max_retry_count = (ENV['MAX_CONNECT_RETRY'] || max_retry).to_i
+      timeout = (ENV['CONNECT_TIMEOUT'] || timeout).to_i
       retry_count = 0
       connected = false
       if ENV['CALABASH_FULL_CONSOLE_OUTPUT'] == '1'
@@ -314,7 +491,7 @@ class Calabash::Cucumber::Launcher
   end
 
   def stop
-    RunLoop.stop(run_loop)
+    RunLoop.stop(run_loop) if run_loop && run_loop[:pid]
   end
 
   def calabash_notify(world)
@@ -366,6 +543,10 @@ class Calabash::Cucumber::Launcher
     ENV['LAUNCH_VIA'] == 'instruments'
   end
 
+  def use_sim_launcher_env?
+    ENV['LAUNCH_VIA'] == 'sim_launcher'
+  end
+
   def reset_between_scenarios?
     ENV['RESET_BETWEEN_SCENARIOS']=="1"
   end
@@ -379,15 +560,19 @@ class Calabash::Cucumber::Launcher
   end
 
   def run_with_instruments?(args)
-    args[:launch_method] == :instruments
+    args && args[:launch_method] == :instruments
   end
 
   def active?
     not run_loop.nil?
   end
 
+  def instruments?
+    !!(active? && run_loop[:pid])
+  end
+
   def inspect
-    msg = ["#{self.class}: Launch Method #{launch_args[:launch_method]}"]
+    msg = ["#{self.class}: Launch Method #{launch_args && launch_args[:launch_method]}"]
     if run_with_instruments?(self.launch_args) && self.run_loop
       msg << "Log file: #{self.run_loop[:log_file]}"
     end
